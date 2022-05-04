035850c3d1580bcb117f8de0d47c13b2c5721716
Arturlima77 <102820245+Arturlima77@users.noreply.github.com>
2022-05-03 23:50:39 -0300

    Pokécheck
    Idealização da tela principal

---
+++ Pokécheck/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+        /// <summary>
+        /// An empty page that can be used on its own or navigated to within a Frame.
+        /// </summary>
+        public sealed partial class MainPage : Page
+        {
+            public MainPage()
+            {
+             this.InitializeComponent();
+            }
+            public object DetailTGrid { get; private set; }
+
+            private void DetailTGrid_FetchCellStyle(object sender, FetchCellStyleEventArgs e)
+            {
+            }
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+
+            }
+
+            private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            {
+
+            }
+
+            private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+            {
+
+            }
+           HttpClient client = new HttpClient();
+           client.GetAsync("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0");
+    }
+    }
+
+
